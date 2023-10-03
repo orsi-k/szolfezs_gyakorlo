fe1010db283204a9de1adfbcc4c7d52805c67b37
orsi-k <kerekes.a.orsolya@gmail.com>
2023-10-03 23:14:57 +0200

    First version of testing intervals

--- src/components/interval-component.tsx
+++ src/components/interval-component.tsx
@@ -1,31 +1,44 @@
+import { useState, useEffect } from "react";
 import { Note, AbcNotation } from "tonal";
 // @ts-ignore
 import { Notation, Midi } from "react-abc";
-import { Card, CardHeader, CardBody, CardFooter } from "@chakra-ui/react";
+import { Card, CardHeader, CardBody, CardFooter, Button } from "@chakra-ui/react";
 
 import "../midi.css";
 
 const IntervalComponent = (props: any) => {
+  const [showInterval, setShowInterval] = useState(!props.isTest);
   const startNote = props.startNote;
   const endNote = Note.get(Note.transpose(startNote, props.interval));
+
+  useEffect(() => {
+    setShowInterval(!props.isTest);
+  }, [props]);
 
   return (
     <div style={{ width: "180px" }}>
       <Card>
-        <CardHeader>{props.interval}</CardHeader>
+        <CardHeader>{showInterval ? props.interval : "??"}</CardHeader>
         <CardBody>
           Starting note: {startNote.pc}
           <br />
-          Ending note: {endNote.pc}
-          <Notation notation={`L: 1\n[${AbcNotation.scientificToAbcNotation(startNote)}${AbcNotation.scientificToAbcNotation(endNote.name)}]`} />
+          Ending note: {showInterval && endNote.pc}
+          <Notation notation={`L: 1\n[${AbcNotation.scientificToAbcNotation(startNote)}${showInterval ? AbcNotation.scientificToAbcNotation(endNote.name) : ""}]`} />
           <div style={{ display: "flex", flexDirection: "row", gap: "5px" }}>
             Together:
-            <Midi notation={`L: 1\n[${AbcNotation.scientificToAbcNotation(startNote)}${AbcNotation.scientificToAbcNotation(endNote.name)}]`} />
+            <Midi
+              key={startNote.pc + props.interval}
+              notation={`L: 1\n[${AbcNotation.scientificToAbcNotation(startNote)}${AbcNotation.scientificToAbcNotation(endNote.name)}]`}
+            />
           </div>
           <div style={{ display: "flex", flexDirection: "row", gap: "5px" }}>
             Separate:
-            <Midi notation={`L: 1/2\n${AbcNotation.scientificToAbcNotation(startNote)}${AbcNotation.scientificToAbcNotation(endNote.name)}`} />
+            <Midi
+              key={startNote.pc + props.interval}
+              notation={`L: 1/2\n${AbcNotation.scientificToAbcNotation(startNote)}${AbcNotation.scientificToAbcNotation(endNote.name)}`}
+            />
           </div>
+          {props.isTest && <Button onClick={() => setShowInterval(true)}>Show</Button>}
         </CardBody>
       </Card>
     </div>
--- src/pages/intervals-page.tsx
+++ src/pages/intervals-page.tsx
@@ -1,21 +1,50 @@
-import { Stack } from "@chakra-ui/react";
-import { Note } from "tonal";
+import { useState } from "react";
+import { Range, Note } from "tonal";
+import { Button, Stack } from "@chakra-ui/react";
 
 import IntervalComponent from "../components/interval-component";
 
 const intervals = ["1P", "2m", "2M", "3m", "3M", "4P", "5P", "6m", "6M", "7m", "7M", "8P"];
+const startingNotes = Range.chromatic(["C3", "C5"]);
 
 const IntervalsPage = () => {
+  const [isTest, setIsTest] = useState(false);
+  const [testInterval, setTestInterval] = useState("");
+  const [startingNote, setStartingNote] = useState(Note.get("C4"));
+
+  const testButtonClicked = () => {
+    if (isTest) setIsTest(false);
+    else {
+      randomizeInterval();
+      setIsTest(true);
+    }
+  };
+
+  const randomizeInterval = () => {
+    setTestInterval(intervals[Math.floor(Math.random() * intervals.length)]);
+    setStartingNote(Note.get(startingNotes[Math.floor(Math.random() * startingNotes.length)]))
+  };
+
   return (
-    <Stack direction="row" wrap="wrap">
-      {intervals.map((interv) => {
-        return (
-          <div key={interv}>
-            <IntervalComponent startNote={Note.get("C4")} interval={interv} isTest={false} />
-          </div>
-        );
-      })}
-    </Stack>
+    <>
+      <Button onClick={testButtonClicked}>Test</Button>
+      {isTest ? (
+        <>
+          <IntervalComponent startNote={startingNote} interval={testInterval} isTest={true} />
+          <Button onClick={randomizeInterval}>New</Button>
+        </>
+      ) : (
+        <Stack direction="row" wrap="wrap">
+          {intervals.map((interv) => {
+            return (
+              <div key={interv}>
+                <IntervalComponent startNote={startingNote} interval={interv} isTest={false} />
+              </div>
+            );
+          })}
+        </Stack>
+      )}
+    </>
   );
 };
 
