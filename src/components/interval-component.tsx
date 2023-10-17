9b8a272e21a86a99d531174dd5ddfd9a452efa6d
orsi-k <kerekes.a.orsolya@gmail.com>
2023-10-17 23:32:50 +0200

    Change long abc notation to shorter ones

--- src/components/interval-component.tsx
+++ src/components/interval-component.tsx
@@ -9,7 +9,9 @@ import "../midi.css";
 const IntervalComponent = (props: any) => {
   const [showInterval, setShowInterval] = useState(!props.isTest);
   const startNote = props.startNote;
+  const abcStartNote = AbcNotation.scientificToAbcNotation(startNote);
   const endNote = Note.get(Note.transpose(startNote, props.interval.en));
+  const abcEndNote = AbcNotation.scientificToAbcNotation(endNote.name);
 
   useEffect(() => {
     setShowInterval(!props.isTest);
@@ -23,22 +25,14 @@ const IntervalComponent = (props: any) => {
           Alaphang: {startNote}
           <br />
           Másik hang: {showInterval && endNote.name}
-          <Notation
-            notation={`L: 1\n[${AbcNotation.scientificToAbcNotation(startNote)} ${showInterval ? AbcNotation.scientificToAbcNotation(endNote.name) : ""}]`}
-          />
+          <Notation notation={`L: 1\n[${abcStartNote} ${showInterval ? abcEndNote : ""}]`} />
           <div style={{ display: "flex", flexDirection: "row", gap: "5px" }}>
             Együtt:
-            <Midi
-              key={startNote + props.interval.en}
-              notation={`L: 1\n [ ${AbcNotation.scientificToAbcNotation(startNote)} ${AbcNotation.scientificToAbcNotation(endNote.name)} ]`}
-            />
+            <Midi key={startNote + props.interval.en} notation={`L: 1\n [ ${abcStartNote} ${abcEndNote} ]`} />
           </div>
           <div style={{ display: "flex", flexDirection: "row", gap: "5px" }}>
             Külön:
-            <Midi
-              key={startNote + props.interval.en}
-              notation={`L: 1/2\n ${AbcNotation.scientificToAbcNotation(startNote)} ${AbcNotation.scientificToAbcNotation(endNote.name)} `}
-            />
+            <Midi key={startNote + props.interval.en} notation={`L: 1/2\n ${abcStartNote} ${abcEndNote} `} />
           </div>
           {props.isTest && <Button onClick={() => setShowInterval(true)}>Show</Button>}
         </CardBody>
--- src/components/triad-component.tsx
+++ src/components/triad-component.tsx
@@ -12,6 +12,11 @@ const TriadComponent = (props: any) => {
   const intervals = Chord.getChord(props.quality.en, startNote.name).intervals;
   const middleNote = Note.get(Note.transpose(startNote, intervals[1])).name;
   const endNote = Note.get(Note.transpose(startNote, intervals[2])).name;
+  const abcNotes = [
+    AbcNotation.scientificToAbcNotation(startNote),
+    AbcNotation.scientificToAbcNotation(middleNote),
+    AbcNotation.scientificToAbcNotation(endNote),
+  ];
 
   useEffect(() => {
     setShowTriad(!props.isTest);
@@ -27,28 +32,14 @@ const TriadComponent = (props: any) => {
           Középső hang: {showTriad && middleNote}
           <br />
           Felső hang: {showTriad && endNote}
-          <Notation
-            notation={`L: 1\n[${AbcNotation.scientificToAbcNotation(startNote)} ${showTriad ? AbcNotation.scientificToAbcNotation(middleNote) : ""} ${
-              showTriad ? AbcNotation.scientificToAbcNotation(endNote) : ""
-            }]`}
-          />
+          <Notation notation={`L: 1\n[${abcNotes[0]} ${showTriad ? abcNotes[1] : ""} ${showTriad ? abcNotes[2] : ""}]`} />
           <div style={{ display: "flex", flexDirection: "row", gap: "5px" }}>
             Együtt:
-            <Midi
-              key={props.quality.en}
-              notation={`L: 1\n [ ${AbcNotation.scientificToAbcNotation(startNote)} ${AbcNotation.scientificToAbcNotation(
-                middleNote
-              )} ${AbcNotation.scientificToAbcNotation(endNote)} ]`}
-            />
+            <Midi key={props.quality.en} notation={`L: 1\n [ ${abcNotes[0]} ${abcNotes[1]} ${abcNotes[2]} ]`} />
           </div>
           <div style={{ display: "flex", flexDirection: "row", gap: "5px" }}>
             Külön:
-            <Midi
-              key={props.quality.en}
-              notation={`L: 1/2\n ${AbcNotation.scientificToAbcNotation(startNote)} ${AbcNotation.scientificToAbcNotation(
-                middleNote
-              )} ${AbcNotation.scientificToAbcNotation(endNote)} `}
-            />
+            <Midi key={props.quality.en} notation={`L: 1/2\n ${abcNotes[0]} ${abcNotes[1]} ${abcNotes[2]} `} />
           </div>
           {props.isTest && <Button onClick={() => setShowTriad(true)}>Show</Button>}
         </CardBody>
